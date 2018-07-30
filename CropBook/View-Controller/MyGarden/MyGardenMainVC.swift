e117934ffab50a1d909fe03ed55a306c6ca9d2f5
jon <jon@Julianas-MacBook-Pro.local>
2018-07-29 22:10:03 -0700

    Modified login system

--- CropBook/View-Controller/MyGarden/MyGardenMainVC.swift
+++ CropBook/View-Controller/MyGarden/MyGardenMainVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 var SHARED_GARDEN_LIST=[MyGarden?]()
 var MY_GARDEN: MyGarden = MyGarden(Name: "My Garden", Address: "")
-
+var SIGNED_IN = false
 @objc protocol gardenButtonClicked{
     func openCrops()
     func postGarden()
@@ -22,6 +22,9 @@ var MY_GARDEN: MyGarden = MyGarden(Name: "My Garden", Address: "")
 class MyGardenMainVC: UIViewController,gardenButtonClicked{
 
     @IBOutlet weak var viewContainer : UIView!
+    @IBOutlet weak var segControl : UISegmentedControl!
+    @IBOutlet weak var signInSignOut : UIButton!
+    
     var views: [UIView]!
     var sharedVC : SharedGardenView = SharedGardenView()
     var myGardenVC : MyGardenView = MyGardenView()
@@ -29,6 +32,10 @@ class MyGardenMainVC: UIViewController,gardenButtonClicked{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let _=Auth.auth().currentUser{
+            self.signInSignOut.setTitle("Sign Out", for: UIControlState.normal)
+        }
         
         addChildViewController(sharedVC)
         addChildViewController(myGardenVC)
@@ -54,12 +61,18 @@ class MyGardenMainVC: UIViewController,gardenButtonClicked{
     
     // Switch between MyGardenView and SharedGardenView
     @IBAction func switchViewAction(_ sender: UISegmentedControl){
-        for i in 0...(views.count-1){
-            self.views[i].isUserInteractionEnabled = (i == sender.selectedSegmentIndex)
+        if let _=Auth.auth().currentUser{
+            for i in 0...(views.count-1){
+                self.views[i].isUserInteractionEnabled = (i == sender.selectedSegmentIndex)
+            }
+            self.viewContainer.bringSubview(toFront: views[sender.selectedSegmentIndex])
+        } else {
+            if sender.selectedSegmentIndex == 1{
+                self.SignIn()
+            }
         }
-        self.viewContainer.bringSubview(toFront: views[sender.selectedSegmentIndex])
+
     }
-    
     
     //Write Garden into Firebase
     func postGarden() {
@@ -96,8 +109,119 @@ class MyGardenMainVC: UIViewController,gardenButtonClicked{
             cropRef.child("ProfName").setValue(profName)
             print("Crop added")
         }
-        
         self.sharedVC.GetOnlineGardens()
+    }
+    
+    @IBAction internal func SignInSelected(_ sender: Any){
+        if let _=Auth.auth().currentUser{
+            try! Auth.auth().signOut()
+            self.signInSignOut.setTitle("Sign In", for: UIControlState.normal)
+        } else {
+            self.SignIn()
+        }
+    }
+    
+    func SignIn(){
+        let alertController = UIAlertController(title: nil, message: "Login/Signup", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Email"
+            textField.keyboardType = .emailAddress
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Password Confirmation"
+            textField.isSecureTextEntry = true
+        }
+        
+        let loginAction = UIAlertAction(title: "Login", style: .default) { (_) in
+            let emailField = alertController.textFields![0]
+            let passwordField = alertController.textFields![1]
+            let confirmPasswordField = alertController.textFields![2]
+
+            //Perform validation or whatever you do want with the text of textfield
+            guard let username = emailField.text   else{return}
+            guard let password = passwordField.text   else{return}
+            guard let confirmPassword = confirmPasswordField.text   else{return}
+            
+            if password != confirmPassword {
+                //passwords don't match
+                alertController.dismiss(animated: true, completion: nil)
+                let alert = UIAlertController(title: "Passwords do not match", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                    alert.dismiss(animated:true, completion:nil)
+                }))
+                self.present(alert, animated:true, completion:nil)
+            }
+            
+            //Login With Firebase
+            Auth.auth().signIn(withEmail: username, password: password){user, error in
+                if error == nil && user != nil{
+                    // signInSuccess
+                    self.signInSignOut.setTitle("Sign Out", for: UIControlState.normal)
+                } else {
+                    print("Error : \(error!.localizedDescription)")
+                    let alert = UIAlertController(title: "Inavlid Username/Password", message: "", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                        alert.dismiss(animated:true, completion:nil)
+                    }))
+                    
+                    self.present(alert, animated:true, completion:nil)
+                }
+            }
+        }
+        
+        let signupAction = UIAlertAction(title: "Signup", style: .default) { (_) in
+            let emailField = alertController.textFields![0]
+            let passwordField = alertController.textFields![1]
+            let confirmPasswordField = alertController.textFields![2]
+            
+            //Perform validation or whatever you do want with the text of textfield
+            guard let username = emailField.text   else{return}
+            guard let password = passwordField.text   else{return}
+            guard let confirmPassword = confirmPasswordField.text   else{return}
+            
+            if password != confirmPassword {
+                //passwords don't match
+                alertController.dismiss(animated: true, completion: nil)
+                let alert = UIAlertController(title: "Passwords do not match", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                    alert.dismiss(animated:true, completion:nil)
+                }))
+                self.present(alert, animated:true, completion:nil)
+            }
+            
+            //Signup With Firebase
+            Auth.auth().createUser(withEmail: username, password: password){user, error in
+                if error == nil && user != nil{
+                    print("User is saved")
+                    self.signInSignOut.setTitle("Sign Out", for: UIControlState.normal)
+                } else {
+                    print("Error : \(error!.localizedDescription)")
+                    let alert = UIAlertController(title: "Inavlid Username/Password", message: "Password must be at least 6 Characters", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                        alert.dismiss(animated:true, completion:nil)
+                    }))
+                    self.present(alert, animated:true, completion:nil)
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){ (_) in
+            self.segControl.selectedSegmentIndex = 0
+        }
+        alertController.addAction(loginAction)
+        alertController.addAction(signupAction)
+        alertController.addAction(cancelAction)
+        
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
     
     func openSharedCrops(index: Int){
@@ -120,7 +244,6 @@ class MyGardenMainVC: UIViewController,gardenButtonClicked{
         // Pass the selected object to the new view controller.
         if (segue.identifier == "showCrops")  {
             let nextController=segue.destination as!GardenCropList
-            //nextController.gardenIndex = gardenIndex!
             nextController.Online=false
         }
         else if (segue.identifier == "showSharedCrops") {
